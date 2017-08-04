dd1263ad99b72710a231cd1c5cbcc85e9d93ec69
ggranga <ggranga@gmail.com>
2017-08-04 10:53:47 +0200

    Improve install_s2download

--- R/install_s2download.R
+++ R/install_s2download.R
@@ -2,6 +2,17 @@
 #' @description s2download is a collection of python scripts used to download
 #'  and correct Sentinel-2 images, and it is required by RSPrePro.
 #'  This function clones them and installs a docker with sen2cor.
+#' @details The function installs both Sentinel-download (to download
+#'  Sentinel-2 images from SciHub), fetchLandsatSentinelFromGoogleCloud
+#'  (to download from Google Cloud) and sen2cor_docker (to run
+#'  sen2cor in a docker which ensure its functionality).
+#'  This last function works only on Linux systems.
+#'
+#'  TODO In future this last function will be separated, in order to
+#'  allow the download to run also on Windows.
+#'
+#'  Note that first run can be very time consuming, since the docker
+#'  for sen2cor have to be downloaded and built.
 #' @param inst_path `character` Path where s2download will be cloned
 #'  (default: a subdirectory of the RSPrePro package).
 #' @return NULL
@@ -9,16 +20,16 @@
 #' @author Luigi Ranghetti, phD (2017) \email{ranghetti.l@@irea.cnr.it}
 #' @note License: GPL 3.0
 #' @export
-#' @importFrom reticulate import import_builtins py_str
+#' @importFrom reticulate import import_builtins py_str use_python py_module_available
 
 install_s2download <- function(inst_path=NA) {
 
-  # import python modules
-  py <- import_builtins(convert=FALSE)
-  sys <- import("sys",convert=FALSE)
-
   # define remote position of s2download
   s2download_git <- "https://github.com/ggranga/s2download.git"
+
+  # define the required python modules and binary dependencies
+  py_modules <- c("os","sys","git","subprocess","re")
+  dependencies <- c("git","docker-compose","python2")
 
   # define inst_path (where to install or update)
   if (is.na(inst_path)) {
@@ -40,15 +51,20 @@ install_s2download <- function(inst_path=NA) {
   }
 
   # check that docker-compose and git are installed
-  for (req_bin in c("git","docker-compose")) {
-    if (Sys.which(req_bin)[1]=="") {
-      print_message(
-        type="error",
-        req_bin," was not found in your system; please install it or update your system PATH.")
-    }
-  }
+  missing_dep <- dependencies[Sys.which(dependencies)==""]
+  if (length(missing_dep)>0) {
+    print_message(
+      type="error",
+      "Some dependencies (",paste(missing_dep,collapse=", "),") were not found in your system; ",
+      "please install them or update your system PATH. ",
+      if ("docker-compose" %in% missing_dep){
+        paste0("\n(Some systems require, after installing docker-compose, to manually enable ",
+               "the service 'docker'; to do this use the command ",
+               "'sudo systemctl enable docker; sudo systemctl start docker')")
+      })
+  } #TODO pip2 not working to install gitPython
 
-  # check that the user is in the "docker" group (root required!)
+  # check the user to be in the "docker" group
   user_groups <- unlist(strsplit(system(paste("groups", system("whoami",intern=TRUE)), intern=TRUE), " "))
   if (!"docker" %in% user_groups) {
     print_message(
@@ -56,28 +72,42 @@ install_s2download <- function(inst_path=NA) {
       "Current user '",system("whoami",intern=TRUE),"' is not in the group 'docker' ",
       "(this is required to run sen2cor in a docker). ",
       "Please add it before installing s2download (you can do it with the command ",
-      "'sudo usermod -a -G docker ",system("whoami",intern=TRUE),"' )")
+      "'sudo usermod -a -G docker ",system("whoami",intern=TRUE),"' ;",
+      "some systems requires to logout and re-login to take effects).")
   }
+
+  # checks the python version
+  # (install_denepdencies.py runs also with python3, but using python2
+  # for compatibility with s2download.py)
+  use_python(Sys.which("python2")[1])
+  py_missing <- py_modules[!sapply(py_modules,py_module_available)]
+  if (length(py_missing)>0) {
+    print_message(
+      type="error",
+      "Some modules (",paste(py_missing,collapse=", "),") are missing in your python distribution. ",
+      "Please install them before continuing (depending on your distribution, you can find ",
+      "packaged version of them, otherwise you can install them manually with ",
+      "'sudo pip2 install ",paste(py_missing,collapse=" "),"' - pip2 is required).")
+  } #TODO pip2 not working to install gitPython
+
+  # import python modules
+  py <- import_builtins(convert=FALSE)
+  sys <- import("sys",convert=FALSE)
 
   # clone the package and import the module
   system(paste0("git clone ",s2download_git," ",inst_path))
-
-
   if (!inst_path %in% py_to_r(sys$path)) {
     sys$path$insert(py$int(0),inst_path)
   }
   install_s2download_dependencies <- import("install_dependencies", convert=FALSE)
-  # TODO: add checks on python modules!
 
   # clone dependent repositories
-
   install_s2download_dependencies$clone_repo(c("ggranga","fetchLandsatSentinelFromGoogleCloud"))
   install_s2download_dependencies$clone_repo(c("ggranga","Sentinel-download"))
   install_s2download_dependencies$clone_sen2cor_docker()
+  install_s2download_dependencies$build_sen2cor_docker()
 
-  # exit
-  print_message(
-    type="message",
-    "s2download and dependencies have been correctly installed.")
+  # TODO check on errors (bot in python some of them does not appear as errors)
+  # and message in case all run ok.
 
 }
