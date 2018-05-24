066f41cf95f18a89badd7fd2415cf05935f3fc86
Luigi Ranghetti <ggranga@gmail.com>
2018-05-24 14:30:29 +0200

    New functions s2_dop() and create_s2_dops()

---
+++ R/create_s2_dop.R
@@ -0,0 +1,100 @@
+#' @title Create the database of S2 orbits and doy of passage
+#' @description The internal function build a database with the base DOY of 
+#'  passage across each Sentinel-2A orbit (which is used in function `s2_dop`).
+#' @param json_path (optional) The path of the output JSON file.
+#'  *Warning*: to create a file which wil be usable by the package,
+#'  this option must be left to NA (default location is within the
+#'  package installation). Edit this only to create the file in another
+#'  place for external use.
+#' @param force (optional) Logical: if FALSE (default), the db is created only
+#'  if missing or not updated; if TRUE, it is created in any case.
+#' @return The path of the json file
+#' @author Luigi Ranghetti, phD (2018) \email{ranghetti.l@@irea.cnr.it}
+#' @note License: GPL 3.0
+#' @import data.table
+#' @importFrom jsonlite toJSON fromJSON
+#' @importFrom utils download.file packageVersion
+
+
+create_s2_dop <- function(json_path = NA, force = FALSE) {
+  
+  # check if the json already exists, and if the version is updated
+  if (is.na(json_path)) {
+    json_path <- file.path(system.file("extdata", package="sen2r"), "doybase.json")
+  }
+  if (system.file("extdata","doybase.json", package="sen2r") == json_path) {
+    if (force == FALSE) {
+      return(json_path)
+    }
+  }
+  
+  # Take products names from Google
+  download.file(
+    "https://storage.googleapis.com/gcp-public-data-sentinel-2/index.csv.gz",
+    s2_store <- tempfile(fileext = ".gz")
+  )
+  s2_all <- fread(paste0("zcat < ", s2_store)) # quite long
+  s2_all[,SENSING_TIME:=as.POSIXct(SENSING_TIME, format="%Y-%m-%dT%H:%M:%S")] # long
+  
+  # Extract data in a sufficient range of time
+  s2_sel <- s2_all[SENSING_TIME>=as.POSIXct("2018-01-01") & SENSING_TIME<as.POSIXct("2018-01-11"),]
+  s2_sel[,utm:=as.integer(substr(GRANULE_ID,6,7))]
+  s2_sel[,orbit:=substr(PRODUCT_ID,35,37)]
+  s2_sel[,mission:=substr(PRODUCT_ID,2,3)]
+  
+  # # Associate the timezone
+  # utm_utc_list <- data.table(matrix(
+  #   c(
+  #     c( 1,-12),
+  #     c( 2,-11), c( 3,-11), c( 4,-11),    c( 5,-10), c( 6,-10),
+  #     c( 7, -9), c( 8, -9), c( 9, -9),    c(10, -8), c(11, -8),
+  #     c(12, -7), c(13, -7), c(14, -7),    c(15, -6), c(16, -6),
+  #     c(17, -5), c(18, -5), c(19, -5),    c(20, -4), c(21, -4),
+  #     c(22, -3), c(23, -3), c(24, -3),    c(25, -2), c(26, -2),
+  #     c(27, -1), c(28, -1), c(29, -1),    c(30,  0), c(31,  0),
+  #     c(32,  1), c(33,  1), c(34,  1),    c(35,  2), c(36,  2),
+  #     c(37,  3), c(38,  3), c(39,  3),    c(40,  4), c(41,  4),
+  #     c(42,  5), c(43,  5), c(44,  5),    c(45,  6), c(46,  6),
+  #     c(47,  7), c(48,  7), c(49,  7),    c(50,  8), c(51,  8),
+  #     c(52,  9), c(53,  9), c(54,  9),    c(55, 10), c(56, 10),
+  #     c(57, 11), c(58, 11), c(59, 11),    c(60, 12)
+  #   ), ncol = 2, dimnames = list(NULL, c("utm", "utc")), byrow = TRUE
+  # ))
+  
+  # # add local timedate
+  # s2_sel[,utc:=utm_utc_list[match(s2_sel$utm, utm_utc_list$utm),"utc"]]
+  # s2_sel[,local_datetime:=SENSING_TIME+3600*utc]
+  
+  # Compute doybase as the first digit of passage of S2A, 
+  # with standard origin (1970-01-01)
+  # (this is sufficient to compute dates of passage, since they are repeated 
+  # with a period of 10 days)
+  prods_nameinfo[,SENSING_TIME:=as.POSIXct(SENSING_TIME)]
+  s2_sel[,date:=as.Date(strftime(SENSING_TIME, "%Y-%m-%d"))]
+  s2_sel[,doybase:=as.integer(date)%%10]
+  s2_sel[mission=="2B",doybase:=(as.integer(date)+5)%%10]
+  
+  # In case of datetime riding on midnight, the day after midnight is considered
+  # (it corresponds to the day in local time)
+  duplicated_orbit <- s2_sel[!duplicated(paste(orbit,doybase)),list(orbit,doybase)][duplicated(orbit),orbit]
+  s2_sel[orbit %in% duplicated_orbit & as.integer(strftime(SENSING_TIME,"%H"))>12, date:=date+1]
+  s2_sel[,doybase:=as.integer(date)%%10] # recompute after changing date
+  s2_sel[mission=="2B",doybase:=(as.integer(date)+5)%%10]
+  
+  # Build database of orbits
+  s2_dop_dt <- s2_sel[
+    !duplicated(paste(doybase,orbit)),# & level=="1C",
+    list(orbit,doybase)
+    ][order(orbit),]
+  
+  # Export
+  json_table <- list(
+    "dop" = s2_dop_dt,
+    "pkg_version" = as.character(packageVersion("sen2r")),
+    "creation_date" = as.character(Sys.time())
+  )
+  writeLines(toJSON(json_table, pretty=TRUE), json_path)
+  
+  return(s2_dop_list)
+  
+}
---
+++ R/s2_dop.R
@@ -0,0 +1,221 @@
+#' @title Return the Dates Of Passage over some orbits
+#' @description The function allows to know which Sentinel-2 passages should
+#'  pass over certain orbits during a defined time interval.
+#'  Dates are intended to be in UTC time.
+#'  Notice that this is the expected calendar: some unexpected events
+#'  (e.g. techical problems, or early working phases during first stages of
+#'  acquisition) could cause the data unavailability even if an 
+#'  acquisition was expected.
+#'  Notice also that some orbits (030, 073 and 116) acquire across UTC midnight:
+#'  in this cases, the date is assumed to be the one of the acquisition after 
+#'  midnight (which corresponds to the date in local time).
+#' @param s2_orbits A vector of Sentinel-2 orbits (as integer numbers 
+#'  or 3-length character).
+#'  Default is all the 143 orbits.
+#' @param timewindow Temporal window for querying: Date object
+#'  of length 1 (single day) or 2 (time window). 
+#'  Is it possible to pass also integer (or difftime) values, which are 
+#'  interpreted as the next n days.
+#'  Also values which can be interpreted as 
+#'  Default is the next 10 days (one cycle).
+#' @param mission (optional) Vector with the desired Sentinel-2 missions
+#'  ("2A", "2B" or both). Default is both.
+#' @return A data table with the dates (column "date"), the missions 
+#' (column "mission") and the orbits (column "orbit").
+#' An empty data table with the same structure is returned if no passages
+#'  were found with the passed settings.
+#' @author Luigi Ranghetti, phD (2018) \email{ranghetti.l@@irea.cnr.it}
+#' @note License: GPL 3.0
+#' @import data.table
+#' @importFrom stringr str_pad
+#' @importFrom jsonlite fromJSON
+#' @importFrom methods is
+#' @export
+#' 
+#' @examples
+#' # All the passages in a cycle of 10 days over all the orbits
+#' s2_dop()
+#' 
+#' # The passages in the current month over two orbits
+#' s2_dop(c("022", "065"), "this month")
+#' 
+#' # The dates in which Sentinel-2A will pass in next six weeks over one orbit 
+#' s2_dop("022", "6 weeks", mission = "2A")$date
+#' 
+#' # All the orbits covered today
+#' s2_dop(timewindow = Sys.Date(), mission = "2B")$orbit
+#' 
+#' # The passages in a fixed time window for one orbit
+#' s2_dop(65, as.Date(c("2018-08-01", "2018-08-31")))
+#' 
+#' # A research with no passages found
+#' s2_dop(22, "2018-08-16", mission = "2A")
+
+
+s2_dop <- function(s2_orbits = 1:143, 
+                   timewindow = "10 days", 
+                   mission = c("2A", "2B")) {
+  
+  # generate doybase.json if missing
+  json_path <- create_s2_dop()
+  s2_dop_dt <- data.table(jsonlite::fromJSON(json_path)$dop)
+  
+  ## Check the length of arguments
+  if (length(s2_orbits) == 0 | length(timewindow) == 0 | length(mission) == 0) {
+    return(
+      data.table(
+        "date" = as.Date(character(0)),
+        "mission" = character(0),
+        "orbit" = character(0), 
+        stringsAsFactors = FALSE
+      )
+    )
+  }
+  
+  ## Check s2_orbits
+  if (is.numeric(s2_orbits)) {
+    s2_orbits <- str_pad(as.integer(s2_orbits), 3, "left", "0")
+  }
+  
+  ## Check mission
+  if (!all(grepl("^2[AB]$", mission))) {
+    print_message(
+      type = "error",
+      "Parameter 'mission' cannot contain values different from \"2A\" and/or \"2B\"."
+    )
+  }
+  
+  ## Check timewindow
+  # check that it does not contain NAs
+  if (anyNA(timewindow)) {
+    print_message(
+      type = "error",
+      "Parameter 'timewindow' cannot contain NA values."
+    )
+  }
+  
+  # Check to be 1 or 2 length
+  if (length(timewindow)>2) {
+    print_message(
+      type = type,
+      "Parameter 'timewindow' must be of length 1 or 2."
+    )
+  }
+  
+  # If difftime, threat as character
+  if (is(timewindow, "difftime")) {
+    timewindow <- paste(timewindow, attr(timewindow, "units"))
+  }
+  
+  # If 1-length numeric, threat as difftime in days
+  if (length(timewindow)==1 & is(timewindow, "numeric")) {
+    timewindow <- as.character(as.integer(timewindow))
+  }
+  
+  # Check character input (to be a date or a time period)
+  if (is(timewindow, "character")) {
+    # if it is a date, convert in Date
+    try_date <- tryCatch({timewindow <- as.Date(timewindow)}, error = function(e){"error"})
+    # otherwise, check if it is a recognised difftime string
+    if (is.character(try_date)) {
+      timewindow <- strsplit(timewindow, " ")[[1]]
+      if (
+        any(!grepl(
+          "^(([0-9]+)|(this)|(days?)|(weeks?)|(months?)|(years?))$", 
+          tolower(timewindow)
+        )) |
+        length(timewindow) > 2
+      ) {
+        print_message(
+          type = "error",
+          "Parameter 'timewindow' is not a recognised string."
+        )
+      }
+      if (length(timewindow)==1) {
+        if (grepl("^(([0-9]+)|(this))$", tolower(timewindow))) {
+          timewindow <- c(timewindow, "days")
+        } else {
+          timewindow <- c(1, timewindow)
+        }
+      }
+      if (timewindow[1]=="this") {
+        timewindow[1] <- 1
+      }
+      timewindow_start <- switch(
+        tolower(gsub("s$","",timewindow[2])),
+        day = Sys.Date(),
+        week = as.Date(cut(Sys.Date(), "week")),
+        month = as.Date(strftime(Sys.Date(),"%Y-%m-01")),
+        year = as.Date(strftime(Sys.Date(),"%Y-01-01"))
+      )
+      timewindow <- seq(timewindow_start, length=2, by=paste(timewindow, collapse=" ")) + c(0,-1)
+      
+    }
+  }
+  
+  # If it is a POSIXct, convert in Date
+  if (is(timewindow, "POSIXt")) {
+    timewindow <- as.Date(strftime(timewindow))
+  }
+  
+  # If it is a 1-length Date, consider as 1-day interval
+  if (length(timewindow)==1 & is(timewindow, "Date")) {
+    timewindow <- rep(timewindow, 2)
+  }
+  
+  # If conversions in Date were not sufficient, stop
+  if (!is(timewindow, "Date")) {
+    print_message(
+      type = type,
+      "Parameter 'timewindow' is not in a recognised format."
+    )
+  }
+  
+  dates_all <- seq(timewindow[1], timewindow[2], by = "day")
+  
+  
+  ## Compute the dates 
+  sel_dop_dt <- s2_dop_dt[orbit %in% s2_orbits,]
+  s2a_dates <- if ("2A" %in% mission) {
+    dates_all[(as.integer(dates_all)%%10) %in% sel_dop_dt$doybase]
+  } else {
+    as.Date(character(0))
+  }
+  s2b_dates <- if ("2B" %in% mission) {
+    dates_all[(as.integer(dates_all)%%10) %in% ((sel_dop_dt$doybase+5)%%10)]
+  } else {
+    as.Date(character(0))
+  }
+  s2a_orbits <- lapply(s2a_dates, function(d) {sel_dop_dt[doybase==as.integer(d)%%10,orbit]})
+  s2b_orbits <- lapply(s2b_dates, function(d) {sel_dop_dt[doybase==(as.integer(d)+5)%%10,orbit]})
+  s2_missions <- c(rep("2A", length(s2a_dates)), rep("2B", length(s2b_dates)))
+  s2_data <- rbindlist(lapply(seq_along(s2_missions), function(i) {
+    expand.grid(
+      "date" = c(s2a_dates, s2b_dates)[i], 
+      "mission" = s2_missions[i], 
+      "orbit" = c(s2a_orbits, s2b_orbits)[[i]], 
+      stringsAsFactors = FALSE
+    )
+  }))
+  
+  if (nrow(s2_data) > 0) {
+    # Order data
+    setorder(s2_data, date, mission, orbit)
+    # Remove unexisting records
+    s2_data <- s2_data[
+      mission == "2A" & date >= "2015-06-27" | 
+        mission == "2B" & date >= "2017-06-29"
+      ]
+    return(s2_data)
+  } else {
+    return(
+      data.table(
+        "date" = as.Date(character(0)),
+        "mission" = character(0),
+        "orbit" = character(0), 
+        stringsAsFactors = FALSE
+      )
+    )
+  }
+  
+}
