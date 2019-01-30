ec9e1273e1766042ed9a96c77ee1ad11f34d2d24
Luigi Ranghetti <ggranga@gmail.com>
2019-01-30 11:52:54 +0100

    New function tiles_intersects
    Now only required tiles are automatically selected (using tiles_intersects() function).

--- R/s2_tiles.R
+++ R/s2_tiles.R
@@ -7,11 +7,14 @@
 #' @author Luigi Ranghetti, phD (2018) \email{ranghetti.l@@irea.cnr.it}
 #' @note License: GPL 3.0
 #' @examples 
+#' # Retrieve all the tiles
 #' s2tiles <- s2_tiles()
 #' 
-#' \dontrun{
+#' # Show a subset of all the tiles
 #' s2tiles_it <- s2tiles[grep("^3[23]T", s2tiles$tile_id),]
-#' mapview::mapview(s2tiles_it)}
+#' s2_coords <- sf::st_coordinates(sf::st_centroid(s2tiles_it))
+#' plot(s2tiles_it$geometry)
+#' text(s2_coords[,1], s2_coords[,2], s2tiles_it$tile_id, cex=0.5)
 
 s2_tiles <- function() {
   
---
+++ R/tiles_intersects.R
@@ -0,0 +1,92 @@
+#' @title Select the tiles intersecting the extent
+#' @description Function which returns the tile IDs of the Sentinel-2 tiles
+#'  which overlap a provided extent.
+#' @param extent `sf` object with the spatial extent.
+#' @param all logical: if TRUE, all the tiles overlapping the extent are
+#'  provided;
+#'  if FALSE (default), unnecessary tiles are skipped.
+#'  Unnecessary tiles are tiles which overlaps the extent for an area already
+#'  coveder by another tile.
+#'  In case the extent is all included in an overlapping area, only one of the
+#'  two candidate tiles is returned (the first in alphabetical order).
+#' @param out_format character: if "sf", the spatial object of the overlapping tiles
+#'  is returned; if "id" (default), a character vector with the tile IDs.
+#' @return the tiles intersecting the extent (see argument `out_format`).
+#' @export
+#' @importFrom sf st_area st_combine st_difference st_intersection 
+#'  st_intersects st_transform st_union
+#' @author Luigi Ranghetti, phD (2019) \email{ranghetti.l@@irea.cnr.it}
+#' @note License: GPL 3.0
+#' @examples
+#' ex_extent <- sf::st_read(
+#'   system.file("extdata/example_files/scalve.kml", package="sen2r"),
+#'   quiet = TRUE
+#' )
+#' ex_extent <- ex_extent[ex_extent$description == "Schilpario",]
+#' 
+#' # Tile ID of the required S2 tile
+#' tiles_intersects(ex_extent)
+#' 
+#' # Tile ID of all the overlapping S2 tiles
+#' tiles_intersects(ex_extent, all = TRUE)
+#' 
+#' # Spatial object with the required tile
+#' sel_tiles <- tiles_intersects(ex_extent, out_format = "sf")
+#' plot(sel_tiles$geometry); plot(ex_extent$geometry, add=TRUE, col="yellow")
+#' 
+#' # Spatial object with the overlapping S2 tiles
+#' sel_tiles <- tiles_intersects(ex_extent, all = TRUE, out_format = "sf")
+#' plot(sel_tiles$geometry); plot(ex_extent$geometry, add=TRUE, col="yellow")
+
+tiles_intersects <- function(extent, all = FALSE, out_format = "id", .s2tiles=NULL) {
+  
+  # Load S2 tiles
+  s2tiles <- if (is.null(.s2tiles)) {
+    s2_tiles()
+  } else {
+    .s2tiles
+  }
+  
+  # Extent to longlat
+  extent <- st_transform(extent,4326)$geometry
+  
+  # Select all the tiles which intersects the extent
+  tiles_intersecting_all <- s2tiles[unique(unlist(suppressMessages(
+    st_intersects(extent, s2tiles)
+  ))),]
+  
+  tiles_intersects <- rep(TRUE, nrow(tiles_intersecting_all))
+  
+  # Cycle on tiles
+  if (all == FALSE) {
+    for (i in rev(seq_len(nrow(tiles_intersecting_all)))) {
+      sel_tile <- tiles_intersecting_all[i,]
+      # intersection between extent and portion of tiles not overlapping other tiles
+      # (with the exception of already discharged ones)
+      sel_tile_notoverlap <- suppressMessages(st_difference(
+        sel_tile$geometry, 
+        st_union(st_combine(
+          tiles_intersecting_all[tiles_intersects & tiles_intersecting_all$tile_id != sel_tile$tile_id,]
+        ))
+      ))
+      # Clip the portion ot the extent exclusive if sel_tile
+      extent_intersection <- suppressMessages(st_intersection(
+        extent,
+        sel_tile_notoverlap
+      ))
+      # TRUE if extent_intersection is not empty, else FALSE
+      tiles_intersects[i] <- all(
+        length(extent_intersection) > 0, 
+        as.numeric(st_area(extent_intersection)) > 0
+      )
+    }
+  }
+  
+  # Return the required output
+  if (out_format == "id") {
+    tiles_intersecting_all[tiles_intersects,]$tile_id
+  } else {
+    tiles_intersecting_all[tiles_intersects,]
+  }
+  
+}
