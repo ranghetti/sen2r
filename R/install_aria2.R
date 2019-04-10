5630f9630a9b64320388d6b6b97dcf735b971ef3
Luigi Ranghetti <ggranga@gmail.com>
2019-04-10 11:30:48 +0200

    Fix wget/aria2 installation

--- R/install_aria2.R
+++ R/install_aria2.R
@@ -15,7 +15,10 @@
 #' }
 
 install_aria2 <- function(aria2_dir = system.file(package="sen2r"), 
-                         force = FALSE) {
+                          force = FALSE) {
+  
+  # define the versions to download (for Windows)
+  aria2_ver <- package_version("1.34.0")
   
   # run only on Windows
   if (Sys.info()["sysname"] != "Windows") {
@@ -39,22 +42,6 @@ install_aria2 <- function(aria2_dir = system.file(package="sen2r"),
     }
   }
   
-  # Check the latest available version
-  system(
-    paste(
-      binpaths$wget, "https://github.com/aria2/aria2/releases/latest", "-O", 
-      aria2_htmlfile <- tempfile()
-    ),
-    intern = Sys.info()["sysname"] == "Windows"
-  )
-  aria2_html <- readLines(aria2_htmlfile)
-  aria2_ver <- sort(package_version(unique(
-    gsub(
-      "^.+release\\-[0-9\\.]+/aria2\\-([0-9\\.]+)\\-win\\-[36][24]bit\\-build1\\.zip.+$", "\\1", 
-      aria2_html[grep("^.+release\\-[0-9\\.]+/aria2\\-([0-9\\.]+)\\-win\\-[36][24]bit\\-build1\\.zip.+$",aria2_html)]
-    )
-  )), decreasing=TRUE)[1]
-  
   # Download aria2
   aria2_url <- paste0(
     "https://github.com/aria2/aria2/releases/download/release-", aria2_ver, 
@@ -65,11 +52,11 @@ install_aria2 <- function(aria2_dir = system.file(package="sen2r"),
   aria2_zip <- normalize_path(file.path(aria2_dir,"aria2.zip"), mustWork=FALSE)
   aria2_path <- normalize_path(file.path(aria2_dir,"aria2c.exe"), mustWork=FALSE)
   
-  # download.file(aria2_url, aria2_zip) # problem with https
-  system(
-    paste(binpaths$wget, aria2_url, "-O", aria2_zip),
-    intern = Sys.info()["sysname"] == "Windows"
-  )
+  download.file(aria2_url, aria2_zip) # problem with https
+  # system(
+  #   paste(binpaths$wget, aria2_url, "-O", aria2_zip),
+  #   intern = Sys.info()["sysname"] == "Windows"
+  # )
   if (file.exists(aria2_zip)) {
     aria2_filelist <- unzip(
       zipfile = aria2_zip,
@@ -77,7 +64,7 @@ install_aria2 <- function(aria2_dir = system.file(package="sen2r"),
     )
     aria2_filename <- aria2_filelist$Name[
       grep(paste0(basename(aria2_path),"$"), aria2_filelist$Name)
-    ]
+      ]
     unzip(
       zipfile = aria2_zip,
       files = aria2_filename,
--- R/install_wget.R
+++ R/install_wget.R
@@ -17,6 +17,9 @@
 install_wget <- function(wget_dir = system.file(package="sen2r"), 
                          force = FALSE) {
   
+  # define the versions to download (for Windows)
+  wget_ver <- package_version("1.19.4")
+  
   # run only on Windows
   if (Sys.info()["sysname"] != "Windows") {
     print_message(
@@ -39,34 +42,32 @@ install_wget <- function(wget_dir = system.file(package="sen2r"),
     }
   }
   
-  # Check the latest available version
-  download.file(
-    "https://eternallybored.org/misc/wget/releases",
-    wget_htmlfile <- tempfile()
+  # Download wget
+  wget_url <- paste0(
+    "https://eternallybored.org/misc/wget/releases/old/wget-", wget_ver, 
+    if (Sys.info()["machine"]=="x86-64") {"-win64"} else {"-win32"}, ".zip"
   )
-  wget_html <- readLines(wget_htmlfile)
-  wget_ver <- sort(package_version(unique(
-    gsub(
-      "^.+>wget\\-([0-9\\.]+)\\-win[36][24]\\.zip<.+$", "\\1", 
-      wget_html[grep("^.+>wget\\-([0-9\\.]+)\\-win[36][24]\\.zip<.+$",wget_html)]
-    )
-  )), decreasing=TRUE)[1]
+  # # Check the latest available version
+  # download.file(
+  #   "https://eternallybored.org/misc/wget/releases",
+  #   wget_htmlfile <- tempfile()
+  # )
+  # wget_html <- readLines(wget_htmlfile)
+  # wget_ver <- sort(package_version(unique(
+  #   gsub(
+  #     "^.+>wget\\-([0-9\\.]+)\\-win[36][24]\\.zip<.+$", "\\1", 
+  #     wget_html[grep("^.+>wget\\-([0-9\\.]+)\\-win[36][24]\\.zip<.+$",wget_html)]
+  #   )
+  # )), decreasing=TRUE)[1]
   
   # Download wget
   wget_url <- paste0(
-    "https://eternallybored.org/misc/wget/releases/wget-", wget_ver, 
-    if (Sys.info()["machine"]=="x86-64") {"-win64"} else {"-win32"}, ".zip"
+    "https://eternallybored.org/misc/wget/", wget_ver,
+    if (Sys.info()["machine"]=="x86-64") {"/64/"} else {"/32/"}, "wget.exe"
   )
-  wget_zip <- normalize_path(file.path(wget_dir,"wget.zip"), mustWork=FALSE)
+  # wget_zip <- normalize_path(file.path(wget_dir,"wget.zip"), mustWork=FALSE)
   wget_path <- normalize_path(file.path(wget_dir,"wget.exe"), mustWork=FALSE)
-  download.file(wget_url, wget_zip)
-  if (file.exists(wget_zip)) {
-    unzip(zipfile = wget_zip,
-          files   = basename(wget_path),
-          exdir   = dirname(wget_path),
-          unzip   = "internal")
-    unlink(wget_zip)
-  }
+  download.file(wget_url, wget_path)
   if (file.exists(wget_path)) {
     binpaths$wget <- wget_path
     writeLines(jsonlite::toJSON(binpaths, pretty=TRUE), attr(binpaths, "path"))
