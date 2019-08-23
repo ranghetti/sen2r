93625fbaf025f825e3037a4e5a1008734d46d3c7
lbusett <lbusett@gmail.com>
2019-08-23 12:10:14 +0200

    Add check_api_creds andd test

---
+++ R/check_api_creds.R
@@ -0,0 +1,25 @@
+#' @title check_api_creds
+#' @description Checks ifprovided username and password are valid Scihub credentials
+#' @param user `string` User Name
+#' @param pwd `string`  password
+#' @return TRUE if credentials are valid, FALSE elsewhere
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  check_api_creds("user", "user")
+#'  }
+#' }
+#' @rdname check_api_creds
+#' @importFrom httr GET authenticate
+check_api_creds <- function(user, pwd) {
+  h1 <- httr::handle("")
+  check_creds <- httr::GET(
+    url = "https://scihub.copernicus.eu/apihub/odata/v1",
+    handle = h1, 
+    httr::authenticate(user, pwd))
+  if (check_creds$status == "401") {
+    FALSE
+  } else {
+    TRUE
+  }
+}
