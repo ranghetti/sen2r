8ebd905a43efd787bc8edb64cd42fb53773110ae
lbusett <lbusett@gmail.com>
2019-08-23 12:11:46 +0200

    same

--- R/check_api_creds.R
+++ R/check_api_creds.R
@@ -10,6 +10,8 @@
 #'  }
 #' }
 #' @rdname check_api_creds
+#' @author Lorenzo Busetto Ranghetti, phD (2019) \email{busetto.l@@irea.cnr.it}
+#' @note License: GPL 3.0
 #' @importFrom httr GET authenticate
 check_api_creds <- function(user, pwd) {
   h1 <- httr::handle("")
