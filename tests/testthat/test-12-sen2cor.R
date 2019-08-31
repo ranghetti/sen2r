757f42f56b1b595dea51d1e044df2f4061f65d95
Luigi Ranghetti <ggranga@gmail.com>
2019-08-31 17:44:03 +0200

    Skip test on sen2cor

--- tests/testthat/test-12-sen2cor.R
+++ tests/testthat/test-12-sen2cor.R
@@ -1,16 +1,23 @@
 context("Test Sen2Cor installation")
 testthat::skip_on_cran()
-# testthat::skip_on_travis()
-
+testthat::skip_on_travis()
 
 testthat::test_that(
-  "Tests on Sen2Cor installation", {
+  "Test that a mismatching Sen2Cor version causes error", {
+    testthat::expect_error(
+      install_sen2cor(version = "2.3.3"),
+      regexp = "[Oo]nly Sen2Cor versions .+ are currently supported"
+    )
+  }
+)
+    
+testthat::test_that(
+  "Test Sen2Cor installation", {
     
     testthat::expect_error(
       install_sen2cor(version = "2.3.3"),
       regexp = "[Oo]nly Sen2Cor versions .+ are currently supported"
     )
-    
     sen2cor_def_version <- package_version("2.8.0")
     sen2cor_inst_dir <- file.path(
       system.file(package="sen2r"),
@@ -28,49 +35,23 @@ testthat::test_that(
       sen2cor_def_version$major,".",sen2cor_def_version$minor,
       "/cfg/L2A_GIPP.xml"
     )))
-    
-    testthat::expect_message(
-      install_sen2cor(),
-      regexp = "Sen2Cor is already installed"
-    )
     
   }
 )
 
-
 testthat::test_that(
-  "Tests on Sen2Cor installation", {
-    
-    testthat::expect_error(
-      install_sen2cor(version = "2.3.3"),
-      regexp = "[Oo]nly Sen2Cor versions .+ are currently supported"
+  "Test that a Sen2Cor reinstallation do not occur if it is already installed", {
+    testthat::expect_message(
+      install_sen2cor(),
+      regexp = "Sen2Cor is already installed"
     )
-    
-    sen2cor_def_version <- package_version("2.8.0")
-    sen2cor_inst_dir <- file.path(
-      system.file(package="sen2r"),
-      paste0("sen2cor_",gsub("\\.","-",sen2cor_def_version))
-    )
-    unlink(sen2cor_inst_dir, recursive=TRUE)
-    
-    install_sen2cor()
-    
-    testthat::expect_true(dir.exists(sen2cor_inst_dir))
-    testthat::expect_true(file.exists(file.path(sen2cor_inst_dir, "bin/L2A_Process")))
-    testthat::expect_false(any(grepl("^Sen2Cor\\-", list.files(sen2cor_inst_dir))))
-    testthat::expect_true(file.exists(paste0(
-      "~/sen2cor/",
-      sen2cor_def_version$major,".",sen2cor_def_version$minor,
-      "/cfg/L2A_GIPP.xml"
-    )))
-    
   }
 )
 
 
 context("Test Sen2Cor")
 testthat::skip_on_cran()
-# testthat::skip_on_travis()
+testthat::skip_on_travis()
 
 # NOTE: these tests require a high amount of time,
 # so running Sen2Cor is disabled by default if SAFE archives are already present.
