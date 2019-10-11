5ed3b65a702ca26facf35dbaef515499e6068bd2
Luigi Ranghetti <ggranga@gmail.com>
2019-10-11 16:21:34 +0200

    New tests

---
+++ tests/testthat/test-t05_load_binpaths.R
@@ -0,0 +1,51 @@
+context("Load empty binaries")
+# testthat::skip_on_cran()
+# testthat::skip_on_travis()
+
+settings_dir <- normalize_path("~/.sen2r", mustWork = FALSE)
+if (dir.exists(settings_dir)) {
+  file.rename(settings_dir, gsub("sen2r$", "sen2r~", settings_dir))
+  restore_settings <- TRUE
+} else {
+  restore_settings <- FALSE
+}
+
+test_that("Load empty binpaths", {
+  binpaths_0 <- load_binpaths()
+  expect_is(binpaths_0, "list")
+  expect_length(binpaths_0, 0)
+  expect_equal(basename(attr(binpaths_0, "path")), "paths.json")
+})
+
+test_that("Try loading Sen2Cor", {
+  testthat::expect_warning(
+    binpaths_1 <- load_binpaths("sen2cor"),
+    "Sen2Cor was not found in your system; you can install it using the function"
+  )
+  expect_equal(binpaths_0, binpaths_1)
+})
+
+
+context("Load aria2 and GDAL")
+testthat::skip_on_cran()
+# testthat::skip_on_travis()
+
+test_that("Load GDAL", {
+  binpaths_2 <- load_binpaths("gdal")
+  expect_is(binpaths_2, "list")
+  expect_length(binpaths_2, 9)
+  expect_equal(binpaths_2$gdalinfo, normalize_path(Sys.which("gdalinfo")))
+  expect_equal(basename(attr(binpaths_2, "path")), "paths.json")
+})
+
+test_that("Load aria2", {
+  binpaths_3 <- load_binpaths("aria2")
+  expect_is(binpaths_3, "list")
+  expect_length(binpaths_3, 10)
+  expect_equal(binpaths_3$aria2, normalize_path(Sys.which("aria2c")))
+  expect_equal(basename(attr(binpaths_2, "path")), "paths.json")
+})
+
+if (restore_settings) {
+  file.rename(gsub("sen2r$", "sen2r~", settings_dir), settings_dir)
+}
---
+++ tests/testthat/test-t08_build_example_param_file.R
@@ -0,0 +1,27 @@
+context("Test build_example_param_file()")
+# testthat::skip_on_cran()
+# testthat::skip_on_travis()
+
+testthat::test_that(
+  "Build an example parameter file", {
+    json_path <- build_example_param_file()
+    testthat::expect_true(file.exists(json_path))
+    list_param <- jsonlite::fromJSON(json_path)
+    testthat::expect_equal(
+      names(list_param),
+      c("preprocess", "s2_levels", "sel_sensor", "online", "order_lta", 
+        "downloader", "overwrite_safe", "rm_safe", "max_cloud_safe",
+        "step_atmcorr", "timewindow", "timeperiod", "extent", 
+        "s2tiles_selected", "s2orbits_selected", "list_prods", "list_indices", 
+        "list_rgb",  "rgb_ranges", "index_source", "mask_type", "max_mask",
+        "mask_smooth", "mask_buffer", "clip_on_extent", "extent_as_mask", 
+        "extent_name", "reference_path", "res", "res_s2", "unit", "proj",
+        "resampling", "resampling_scl", "outformat", "rgb_outformat",
+        "index_datatype", "compression", "rgb_compression", "overwrite", 
+        "path_l1c", "path_l2a", "path_tiles", "path_merged", "path_out", 
+        "path_rgb", "path_indices", "path_subdirs", "thumbnails", "log", 
+        "parallel", "processing_order", "pkg_version"
+      )
+    )
+  }
+)
