e1a03d126b8129f52f6517fdb445f7f60e88fc3d
Luigi Ranghetti <ggranga@gmail.com>
2019-10-24 14:16:38 +0200

    Do not use dhus but apihub also for orders
    I noticed that, using dhus, after ordering a single SAFE the user quota exceeded; instead, with apihub that does not occur (for now). At the time I am writing, on the ESA SciHub interface LTA products are not listed, while s2_list shows them.

--- R/s2_order.R
+++ R/s2_order.R
@@ -116,8 +116,8 @@ s2_order <- function(
     s2_prodlist <- unlist(fromJSON(s2_prodlist))
   }
   
-  # replace apihub with dhus
-  s2_prodlist <- gsub("apihub", "dhus", s2_prodlist)
+  # # replace apihub with dhus
+  # s2_prodlist <- gsub("apihub", "dhus", s2_prodlist)
   
   # read credentials
   creds <- read_scihub_login(apihub)
