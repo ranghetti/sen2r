8deedf9d317a0d6ca091582efe7c6fff9a170589
Luigi Ranghetti <ggranga@gmail.com>
2019-11-28 16:44:45 +0100

    Skip on Windows

--- tests/testthat/test-t36_mountpoint.R
+++ tests/testthat/test-t36_mountpoint.R
@@ -2,20 +2,21 @@ context("Test mountpoint()")
 testthat::skip_on_cran()
 testthat::skip_on_travis()
 
-testthat::test_that(
-  "Mountpoint home", {
-    mp_1 <- mountpoint(".")
-    testthat::expect_is(mp_1, "character")
-    testthat::expect_true(dir.exists(mp_1))
-    testthat::expect_equal(names(attributes(mp_1)), "protocol")
-  }
-)
-
-testthat::test_that(
-  "Mountpoint package home", {
-    mp_2 <- mountpoint(system.file(package = "sen2r"))
-    testthat::expect_is(mp_2, "character")
-    testthat::expect_true(dir.exists(mp_2))
-    testthat::expect_equal(names(attributes(mp_2)), "protocol")
-  }
-)
+if (Sys.info()["sysname"] != "Windows") {
+  testthat::test_that(
+    "Mountpoint home", {
+      mp_1 <- mountpoint(".")
+      testthat::expect_is(mp_1, "character")
+      testthat::expect_true(dir.exists(mp_1))
+      testthat::expect_equal(names(attributes(mp_1)), "protocol")
+    }
+  )
+  testthat::test_that(
+    "Mountpoint package home", {
+      mp_2 <- mountpoint(system.file(package = "sen2r"))
+      testthat::expect_is(mp_2, "character")
+      testthat::expect_true(dir.exists(mp_2))
+      testthat::expect_equal(names(attributes(mp_2)), "protocol")
+    }
+  )
+}
