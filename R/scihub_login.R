c4b9494272e42500ccaf6e6464384bb2e72984bc
lbusett <lbusett@gmail.com>
2019-12-10 14:58:07 +0100

    Improve documentation on "new" credentials problem
    Add explanation in documentation and provide a more verbose error message, concerning the problem of "delayed" recognition of scihub credentials.

--- R/scihub_login.R
+++ R/scihub_login.R
@@ -11,11 +11,17 @@
 #' @param password SciHub password.
 #' @return `read_scihub_login` returns a matrix of credentials,
 #'  in which `username` is in the first column, `password` in the second.
+#' @details Notice that new/recently updated SciHub credentials are recognised by API Hub
+#'  with a delay of about one week (see \url{https://scihub.copernicus.eu/twiki/do/view/SciHubWebPortal/APIHubDescription} for details).
+#'
+#' For this reason, newly created credentials can not immediately be ued by `sen2r`, 
+#' and password edits on old credentials are not immediately recognised.
 #' @author Luigi Ranghetti, phD (2019) \email{luigi@@ranghetti.info}
 #' @note License: GPL 3.0
 #' @importFrom reticulate py_to_r
 #' @importFrom shiny a actionButton icon modalButton modalDialog passwordInput tagList textInput
 #' @importFrom shinyFiles shinyFileSave
+#' 
 #' @examples
 #'   check_scihub_login("user", "user")
 #'   write_scihub_login("user", "user")
@@ -116,7 +122,11 @@ write_scihub_login <- function(username, password,
       print_message(
         type = "error",
         "The provided credentials are not valid, ",
-        "so the will not be saved."
+        "so they will not be saved.\n",
+        "Please notice that new/recently updated SciHub credentials are recognised by API Hub ",
+        "with a delay of about one week.\n",
+        "For this reason, newly created SciHub credentials can not immediately be used by `sen2r`", 
+        "and password edits on old credentials are not immediately recognised."
       )
     }
   }
