ranghetti/sen2r

commit a774498ab9c3db02ffbf8597f5cc6a740610a39d

Change output of s2_download()
Now s2_download() returns the safelist of downloaded/existing SAFE archives, instead than NULL. These are different from inputs when the downloaded SAFE name is dfifferent from the required (i.e. different processing date with products ordered from LTA).

R/s2_download.R
@@ -20,9 +20,10 @@
 #'  if FALSE, they are simply skipped.
 #' @param overwrite Logical value: should existing output archives be
 #'  overwritten? (default: FALSE)
-#' @return NULL (the function is called for its side effects)
+#' @return Vector character with the list ot the output products
+#'  (being downloaded or already existing).
 #'
-#' @author Luigi Ranghetti, phD (2019) \email{luigi@@ranghetti.info}
+#' @author Luigi Ranghetti, phD (2020) \email{luigi@@ranghetti.info}
 #' @author Lorenzo Busetto, phD (2019) \email{lbusett@@gmail.com}
 #' @note License: GPL 3.0
 #' @importFrom httr GET RETRY authenticate progress write_disk
@@ -106,6 +107,7 @@ s2_download <- function(
   # check input format
   s2_prodlist <- as(s2_prodlist, "safelist")
   # TODO add input checks
+  s2_meta <- safe_getMetadata(s2_prodlist, info = "nameinfo")
   
   # read credentials
   creds <- read_scihub_login(apihub)
@@ -131,23 +133,30 @@ s2_download <- function(
   } else {
     .s2_availability
   }
-
+  
   
   # Order products stored from the Long Term Archive
   if (order_lta == TRUE) {
     ordered_products <- .s2_order(s2_prodlist, .s2_availability = s2_availability)
   }
-
+  
   
   ## Download products available for download
   
-  for (i in which(s2_availability)) {
+  safe_prodlist <- foreach(i = which(s2_availability), .combine = c) %do% {
     
     link <- s2_prodlist[i]
     zip_path <- file.path(outdir, paste0(names(s2_prodlist[i]),".zip"))
     safe_path <- gsub("\\.zip$", "", zip_path)
     
-    if (any(overwrite == TRUE, !file.exists(safe_path))) {
+    # regular expression to detect if equivalent products already exist
+    safe_regex <- s2_meta[i,paste0(
+      "^S",mission,"\\_MSIL",level,"\\_",strftime(sensing_datetime,"%Y%m%dT%H%M%S"),
+      "\\_N[0-9]{4}\\_R",id_orbit,"\\_T",id_tile,"\\_[0-9]{8}T[0-9]{6}\\.SAFE$"
+    )]
+    safe_existing <- list.files(dirname(zip_path), safe_regex, full.names = TRUE)
+    
+    if (any(overwrite == TRUE, length(safe_existing) == 0)) {
       
       print_message(
         type = "message",
@@ -188,7 +197,7 @@ s2_download <- function(
         })
         
       }
-
+      
       # check if the user asked to download a LTA product
       download_is_lta <- if (inherits(download, "response")) {
         download$status_code == 202
@@ -237,6 +246,9 @@ s2_download <- function(
         if (dir.exists(safe_path)) {
           unlink(safe_path, recursive = TRUE)
         }
+        # update SAFE name (possible different processing date)
+        zip_content <- unzip(zip_path, list = TRUE)
+        safe_newname <- unique(gsub("(^[^\\/]+)\\/.*$", "\\1", zip_content$Name))
         # unzip
         unzip(zip_path, exdir = dirname(zip_path))
         file.remove(zip_path)
@@ -252,10 +264,18 @@ s2_download <- function(
         "since the corresponding folder already exists."
       )
       
+      safe_existing_meta <- safe_getMetadata(safe_existing, info = "nameinfo")
+      safe_newname <- safe_existing_meta$name[
+        order(nn(safe_existing_meta$creation_datetime), decreasing = TRUE)[1]
+        ]
+      
     }
+    
+    # return to foreach
+    as(setNames(link, safe_newname), "safelist")
     
   }
   
-  return(invisible(NULL))
+  return(safe_prodlist)
   
 }

R/safe_getMetadata.R
@@ -823,9 +823,6 @@ safe_isvalid <- function(s2, allow_oldnames = FALSE, check_file = TRUE) {
         as.POSIXct(metadata_dt$creation_datetime, format = "%s"), tz = "UTC", usetz = TRUE
       )
     }
-    if (!is.null(metadata_dt$utm)) {
-      metadata_dt$utm <- as.integer(metadata_dt$utm)
-    }
     if (format == "data.frame") {
       data.frame(metadata_dt)
     } else if (format == "data.table") {

R/sen2cor.R
@@ -70,8 +70,8 @@
 #'  (it is used when the function is called by `sen2r()`).
 #' @param .log_output (optional) Internal parameter
 #'  (it is used when the function is called by `sen2r()`).
-#' @return Vector character with the list ot the output products (being corrected or already
-#'  existing)
+#' @return Vector character with the list ot the output products
+#'  (being corrected or already existing).
 #'
 #' @author Luigi Ranghetti, phD (2019) \email{luigi@@ranghetti.info}
 #' @note License: GPL 3.0

R/sen2r.R
@@ -1719,7 +1719,7 @@ sen2r <- function(param_list = NULL,
             }
           }
           
-          s2_download(
+          s2_downloaded_l2a <- s2_download(
             s2_to_download,
             outdir = path_l2a,
             downloader = pm$downloader,
@@ -1804,8 +1804,8 @@ sen2r <- function(param_list = NULL,
             
           }
           
-          s2_download(
-            sel_s2_list_l1c[!names(sel_s2_list_l1c) %in% list.files(path_l1c, "\\.SAFE$")],
+          s2_downloaded_l1c <- s2_download(
+            s2_to_download,
             outdir = path_l1c,
             downloader = pm$downloader,
             overwrite = pm$overwrite_safe
@@ -1892,6 +1892,25 @@ sen2r <- function(param_list = NULL,
           )]
         ),
         ]
+      
+      if (pm$online == TRUE) {
+        # replace SAFE names changed after download (i.e. updated creation date)
+        sel_s2_dt_id <- sel_s2_dt[
+          ,paste(mission, level, sensing_datetime, id_orbit, id_tile)
+          ]
+        s2_downloaded_id <- c(
+          safe_getMetadata(s2_downloaded_l1c, info = "nameinfo")[
+            ,paste(mission, level, sensing_datetime, id_orbit, id_tile)
+            ],
+          safe_getMetadata(s2_downloaded_l2a, info = "nameinfo")[
+            ,paste(mission, level, sensing_datetime, id_orbit, id_tile)
+            ]
+        )
+        sel_s2_dt[
+          match(s2_downloaded_id, sel_s2_dt_id), 
+          name:=names(c(s2_downloaded_l1c,s2_downloaded_l2a))
+          ]
+      }
       
       # redefine sel_s2_list_l1c/l2a
       sel_s2_list_l1c <- sel_s2_dt[level=="1C",url] # list of required L1C

man/s2_download.Rd
@@ -40,7 +40,8 @@ if FALSE, they are simply skipped.}
 overwritten? (default: FALSE)}
 }
 \value{
-NULL (the function is called for its side effects)
+Vector character with the list ot the output products
+(being downloaded or already existing).
 }
 \description{
 The function downloads S2 products.
@@ -75,7 +76,7 @@ s2_download(list_safe, outdir=tempdir())
 }
 }
 \author{
-Luigi Ranghetti, phD (2019) \email{luigi@ranghetti.info}
+Luigi Ranghetti, phD (2020) \email{luigi@ranghetti.info}
 
 Lorenzo Busetto, phD (2019) \email{lbusett@gmail.com}
 }

man/sen2cor.Rd
@@ -104,8 +104,8 @@ possible (e.g. \code{parallel = 4}).}
 (it is used when the function is called by \code{sen2r()}).}
 }
 \value{
-Vector character with the list ot the output products (being corrected or already
-existing)
+Vector character with the list ot the output products
+(being corrected or already existing).
 }
 \description{
 The function uses Sen2Cor to manually correct L1C products.

tests/testthat/test-t12_s2_download.R
@@ -25,14 +25,15 @@ testthat::test_that(
       "S2A_MSIL2A_20170703T101021_N0205_R022_T32TNR_20170703T101041.SAFE" = 
         "https://scihub.copernicus.eu/apihub/odata/v1/Products('b4b6b897-a03a-4f3c-b78d-974066574aed')/$value"
     )
-    suppressWarnings(s2_download(
+    suppressWarnings(s2_l2a_downloaded <- s2_download(
       s2_l2a_list,
       downloader = "builtin",
       outdir = safe_dir,
       overwrite = test_download
     )) # suppressWarnings used to manage possible warnings for skept Md5sum checks
-    exp_outsafe_1 <- file.path(safe_dir, names(s2_l2a_list))
+    exp_outsafe_1 <- file.path(safe_dir, names(s2_l2a_downloaded))
     testthat::expect_true(all(file.exists(exp_outsafe_1)))
+    testthat::expect_equal(length(s2_l2a_downloaded), length(s2_l2a_list))
     
     # test raster metadata
     exp_meta_ex <- raster_metadata(file.path(
@@ -68,7 +69,7 @@ testthat::test_that(
       testthat::expect_equal(as.Date(safe_metadata1$sensing_datetime), rep(as.Date("2017-07-03"),2))
       testthat::expect_equal(safe_metadata1$id_orbit, rep("022",2))
       testthat::expect_equal(safe_metadata1$id_tile, c("32TNS","32TNR"))
-      testthat::expect_equal(safe_metadata1$utm, rep(32,2))
+      testthat::expect_equal(safe_metadata1$utm, rep("32N",2))
       testthat::expect_equal(safe_metadata1$direction, rep("DESCENDING",2))
       testthat::expect_equal(safe_metadata1$orbit_n, rep("22",2))
       testthat::expect_equal(safe_metadata1$nodata_value, rep("0",2))
@@ -118,14 +119,15 @@ testthat::test_that(
       "S2A_MSIL1C_20170703T101021_N0205_R022_T32TNS_20170703T101041.SAFE" = 
         "https://scihub.copernicus.eu/apihub/odata/v1/Products('5f590bcb-ee55-4a20-8e75-bde99f5b93d4')/$value"
     )
-    suppressWarnings(s2_download(
+    suppressWarnings(s2_l2a_downloaded <- s2_download(
       s2_l1c_list,
       downloader = "aria2",
       outdir = safe_dir,
       overwrite = test_download
     )) # suppressWarnings used to manage possible warnings for skept Md5sum checks
-    exp_outsafe_2 <- file.path(safe_dir, names(s2_l1c_list))
+    exp_outsafe_2 <- file.path(safe_dir, names(s2_l2a_downloaded))
     testthat::expect_true(all(file.exists(exp_outsafe_2)))
+    testthat::expect_equal(length(s2_l2a_downloaded), length(s2_l1c_list))
     
     # test raster metadata
     exp_meta_ex <- raster_metadata(file.path(
@@ -162,7 +164,7 @@ testthat::test_that(
       testthat::expect_equal(unique(safe_metadata$level), "1C")
       testthat::expect_equal(unique(safe_metadata$id_orbit), "022")
       testthat::expect_equal(unique(safe_metadata$id_tile), c("32TNR", "32TNS"))
-      testthat::expect_equal(unique(safe_metadata$utm), 32)
+      testthat::expect_equal(unique(safe_metadata$utm), "32N")
       testthat::expect_equal(unique(safe_metadata$direction), "DESCENDING")
       testthat::expect_equal(unique(safe_metadata$orbit_n), "22")
       testthat::expect_equal(unique(safe_metadata$nodata_value), "0")

tests/testthat/test-t14_sen2cor.R
@@ -115,7 +115,7 @@ if (test_sen2cor) {
       testthat::expect_equal(safe_metadata$id_orbit, "022")
       testthat::expect_equal(safe_metadata$id_tile, "32TNS")
       testthat::expect_equal(safe_metadata$tiles, "32TNS")
-      testthat::expect_equal(safe_metadata$utm, 32)
+      testthat::expect_equal(safe_metadata$utm, "32N")
       testthat::expect_equal(safe_metadata$direction, "DESCENDING")
       testthat::expect_equal(safe_metadata$orbit_n, "22")
       
@@ -184,7 +184,7 @@ if (test_sen2cor) {
       testthat::expect_equal(safe_metadata$id_orbit, rep("022",2))
       testthat::expect_equal(safe_metadata$id_tile, c("32TNR","32TNS"))
       testthat::expect_equal(safe_metadata$tiles, c("32TNR","32TNS"))
-      testthat::expect_equal(safe_metadata$utm, rep(32,2))
+      testthat::expect_equal(safe_metadata$utm, rep("32N",2))
       testthat::expect_equal(safe_metadata$direction, rep("DESCENDING",2))
       testthat::expect_equal(safe_metadata$orbit_n, rep("22",2))
       

commit c7d6b239eb35362f505c785563c95fbc4674d0eb

Fix download with aria2c
Simnple fix to avoid crash on download when using aria due to changes to s2_download introduced in a774498ab9c3db02ffbf8597f5cc6a740610a39d

(was failing in creating a safelist on "links" with added backslashes used on aria download on linux, such as  https://scihub.copernicus.eu/apihub/odata/v1/Products('f3d1966b-97c6-4448-9465-fbd6905d132a')/\\$value
)

R/s2_download.R
@@ -180,13 +180,15 @@ s2_download <- function(
         
         binpaths <- load_binpaths("aria2")
         if (Sys.info()["sysname"] != "Windows") {
-          link <- gsub("/\\$value", "/\\\\$value", link)
+          link_aria <- gsub("/\\$value", "/\\\\$value", link)
+        } else {
+          link_aria <- link
         }
         aria_string <- paste0(
           binpaths$aria2c, " -x 2 --check-certificate=false -d ",
           dirname(zip_path),
           " -o ", basename(zip_path),
-          " ", "\"", as.character(link), "\"",
+          " ", "\"", as.character(link_aria), "\"",
           " --allow-overwrite --file-allocation=none --retry-wait=2",
           " --http-user=", "\"", creds[1], "\"",
           " --http-passwd=", "\"", creds[2], "\"",