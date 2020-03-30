1132ae755ddf2d656f39e255805db60005e8a9b2
Luigi Ranghetti <ggranga@gmail.com>
2020-03-30 13:35:17 +0200

    Compare CRSs without using EPSG

--- tests/testthat/test-t34_st_crs2.R
+++ tests/testthat/test-t34_st_crs2.R
@@ -1,11 +1,25 @@
 context("Test st_crs2")
 
+# convenience function to compare non-null CRSs
+# without using EPSG (so usable with rgdal >= 1.5)
+expect_equal_crs <- function(crs1, crs2) {
+  ref_vec <- st_geometry(st_read(
+    system.file("extdata/vector/barbellino.geojson", package = "sen2r"), 
+    quiet = TRUE
+  ))
+  testthat::expect_equal(
+    sf::st_coordinates(sf::st_transform(ref_vec, crs1)), 
+    sf::st_coordinates(sf::st_transform(ref_vec, crs2))
+  )
+}
+
+
 testthat::test_that(
   "st_crs2, input EPSG", {
-    testthat::expect_equal(st_crs2(9)$epsg, 32609)
-    testthat::expect_equal(st_crs2("EPSG:32609")$epsg, 32609)
-    testthat::expect_equal(st_crs2("9N")$epsg, 32609)
-    testthat::expect_equal(st_crs2("9S")$epsg, 32709)
+    expect_equal_crs(st_crs2(9), 32609)
+    expect_equal_crs(st_crs2("EPSG:32609"), 32609)
+    expect_equal_crs(st_crs2("9N"), 32609)
+    expect_equal_crs(st_crs2("9S"), 32709)
   }
 )
 
@@ -15,11 +29,11 @@ testthat::test_that(
       "extdata/out/S2A2A_20190723_022_Barbellino_BOA_10.tif", 
       package="sen2r"
     )
-    testthat::expect_equal(st_crs2(raster_path)$epsg, 32632)
+    expect_equal_crs(st_crs2(raster_path), 32632)
     # This test was skipped while stars is being incompatible with sf >= 0.9,
     # and will be restored after stars will have been fixed (#295).
     if (packageVersion("sf") < 0.9) {
-      testthat::expect_equal(st_crs2(stars::read_stars(raster_path))$epsg, 32632)
+      expect_equal_crs(st_crs2(stars::read_stars(raster_path)), 32632)
     }
     # This test was skipped while raster is reading PROJ.4 instead than WKT,
     # and so the EPSG is being lost (#295).
@@ -39,13 +53,13 @@ testthat::test_that(
     )
     # 
     if (packageVersion("sf") < 0.9) {
-      testthat::expect_equal(st_crs2(raster_path)$epsg, 32632)
-      testthat::expect_equal(st_crs2(stars::read_stars(raster_path))$epsg, 32632)
+      expect_equal_crs(st_crs2(raster_path), 32632)
+      expect_equal_crs(st_crs2(stars::read_stars(raster_path)), 32632)
     }
     # testthat::expect_equal(st_crs2(raster::raster(raster_path))$epsg, 32632)
-    testthat::expect_equal(st_crs2(vector_path)$epsg, 32632)
-    testthat::expect_equal(st_crs2(sf::read_sf(vector_path))$epsg, 32632)
-    testthat::expect_equal(st_crs2(as(sf::read_sf(vector_path), "Spatial"))$epsg, 32632)
+    expect_equal_crs(st_crs2(vector_path), 32632)
+    expect_equal_crs(st_crs2(sf::read_sf(vector_path)), 32632)
+    expect_equal_crs(st_crs2(as(sf::read_sf(vector_path), "Spatial")), 32632)
   }
 )
 
@@ -66,14 +80,14 @@ testthat::test_that(
   "st_crs2, input WKT", {
     wkt_32n <- st_as_text_2(st_crs(32609))
     writeLines(wkt_32n, wkt_32n_path <- tempfile())
-    testthat::expect_equal(st_crs2(wkt_32n)$epsg, 32609)
-    testthat::expect_equal(st_crs2(wkt_32n_path)$epsg, 32609)
+    expect_equal_crs(st_crs2(wkt_32n), 32609)
+    expect_equal_crs(st_crs2(wkt_32n_path), 32609)
   }
 )
 
 testthat::test_that(
   "st_crs2, input PROJ.4", {
-    testthat::expect_equal(st_crs2("+init=epsg:32609")$epsg, 32609)
+    expect_equal_crs(st_crs2("+init=epsg:32609"), 32609)
     gdal_version <- package_version(gsub(
       "^.*GDAL ([0-9\\.]+)[^0-9].*$", "\\1",
       system(paste0(load_binpaths("gdal")$gdalinfo," --version"), intern = TRUE)
@@ -84,8 +98,8 @@ testthat::test_that(
         "Using PROJ\\.4 strings is deprecated with"
       )
     } else {
-      testthat::expect_equal(
-        st_crs2("+proj=utm +zone=9 +datum=WGS84 +units=m +no_defs")$epsg,
+      expect_equal_crs(
+        st_crs2("+proj=utm +zone=9 +datum=WGS84 +units=m +no_defs"),
         32609
       )
     }
