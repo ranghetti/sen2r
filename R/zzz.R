53328c19cb4e8622526a73d61fca6e847f19b02a
Luigi Ranghetti <6874239+ranghetti@users.noreply.github.com>
2020-05-15 16:49:13 +0200

    Don't show welcome message if not needed

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,17 @@
 .onAttach <- function(libname, pkgname) {
+
+  # If sen2r settings dir does not exist, ask for creating it
+  invisible(give_write_permission())
+
+  # Welcome message
   packageStartupMessage(paste(
     "Welcome to sen2r. To use the package from a GUI, launch",
     " > sen2r()",
     "Documentation: http://sen2r.ranghetti.info",
-    if (Sys.info()["sysname"] %in% c("Windows", "Darwin")) {
+    if (all(
+      Sys.info()["sysname"] %in% c("Windows", "Darwin"),
+      length(load_binpaths()) == 0
+    )) {
       paste(
         "\nIMPORTANT: sen2r depends on some external tools;",
         "before using it, it is strongly recommended to run function",
@@ -15,6 +23,4 @@
     sep = "\n"
   ))
   
-  invisible(give_write_permission())
-
 }
