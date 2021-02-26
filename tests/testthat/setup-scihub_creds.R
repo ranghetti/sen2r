b9588d84353de7bf57bee70533228b59aa3970b0
Luigi Ranghetti <ggranga@gmail.com>
2021-02-26 14:30:47 +0100

    Use GitHub secrets

--- tests/testthat/setup-scihub_creds.R
+++ tests/testthat/setup-scihub_creds.R
@@ -1,5 +1,10 @@
 # Retrieve Travis credentials
-tests_apihub <- try(sen2r::read_scihub_login(), silent = TRUE)
+tests_apihub <- if (Sys.getenv("GITHUB_ACTIONS") == "true") {c(
+  paste0("sen2r_travis_",Sys.getenv("R_VERSION_STRING")),
+  Sys.getenv("CI_PASSWORD")
+)} else {
+  saved_creds <- try(sen2r::read_scihub_login(), silent = TRUE)
+}
 
 if (!inherits(tests_apihub, "try-error")) {
   
