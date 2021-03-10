fa73e42549ea91ba11738a41454007077eac391a
Luigi Ranghetti <ggranga@gmail.com>
2021-03-10 17:27:28 +0100

    WIP

--- tests/testthat/setup-commons.R
+++ tests/testthat/setup-commons.R
@@ -2,3 +2,7 @@
 ref_dir <- system.file("extdata/out", package = "sen2r")
 safe_dir <- file.path(dirname(attr(load_binpaths(), "path")), "safe")
 dir.create(safe_dir, showWarnings = FALSE)
+
+# Silence clock check (https://stackoverflow.com/questions/63613301/r-cmd-check-note-unable-to-verify-current-time)
+Sys.setenv('_R_CHECK_SYSTEM_CLOCK_' = 0)
+
--- tests/testthat/setup-scihub_creds.R
+++ tests/testthat/setup-scihub_creds.R
@@ -3,10 +3,10 @@ tests_apihub <- if (Sys.getenv("GITHUB_ACTIONS") == "true") {c(
   paste0("sen2r_travis_",Sys.getenv("R_VERSION_STRING")),
   Sys.getenv("CI_PASSWORD")
 )} else {
-  saved_creds <- try(sen2r::read_scihub_login(), silent = TRUE)
+  try(sen2r::read_scihub_login(), silent = TRUE)
 }
 
-if (!inherits(saved_creds, "try-error")) {
+if (!inherits(tests_apihub, "try-error")) {
   
   # Use a temporary apihub file
   write_scihub_login(
