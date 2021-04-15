127fb9faf717565841f9f5c0a6090db2bccd0b76
Luigi Ranghetti <ggranga@gmail.com>
2021-04-15 14:04:24 +0200

    Don't run some examples

--- R/gipp.R
+++ R/gipp.R
@@ -177,7 +177,7 @@ read_gipp <- function(gipp_names, gipp_path = NA) {
 #' @return [set_gipp()] returns NULL (the function is called for its side effects).
 #' @export
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' if (!is.null(load_binpaths()$sen2cor)) {
 #' # Read default values
 #' read_gipp(c("dem_directory", "dem_reference"))
