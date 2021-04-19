53540ced075c231c8529b15fd910966c1f985649
Luigi Ranghetti <ggranga@gmail.com>
2021-04-19 15:34:35 +0200

    WIP

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,3 +1,4 @@
 if(requireNamespace('spelling', quietly = TRUE))
-spelling::spell_check_test(vignettes = TRUE, error = FALSE,
-                           skip_on_cran = TRUE)
+  # spelling::spell_check_test(vignettes = TRUE, error = FALSE,
+  #                            skip_on_cran = TRUE)
+  spelling::spell_check_package()
