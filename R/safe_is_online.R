460c45f1a3704ca545915dc747b552a2a42b950a
Luigi Ranghetti <ggranga@gmail.com>
2021-05-01 15:37:05 +0200

    Fix previous commit

--- R/safe_is_online.R
+++ R/safe_is_online.R
@@ -58,7 +58,11 @@ safe_is_online <- function(s2_prodlist = NULL, apihub = NA, verbose = TRUE) {
   # TODO add input checks
   
   # replace apihub with dhus
-  s2_prodlist <- gsub("apihub", "dhus", s2_prodlist)
+  s2_prodlist <- gsub(
+    "((scihub)|(apihub))\\.copernicus\\.eu/apihub", 
+    "scihub.copernicus.eu/dhus", 
+    s2_prodlist
+  )
   
   # read credentials
   creds <- read_scihub_login(apihub)
