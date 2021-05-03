c89d6feb12b98490790ed757c203a8d8f35e0d78
Luigi Ranghetti <ggranga@gmail.com>
2021-05-03 15:04:14 +0200

    WIP errors on GitHub Actions

--- tests/testthat/setup-dependencies.R
+++ tests/testthat/setup-dependencies.R
@@ -3,3 +3,8 @@
 # if (Sys.info()["sysname"] == "Windows") {sen2r::install_aria2()}
 if (Sys.info()["sysname"] == "Linux") {sen2r::check_gdal()}
 s2tiles <- s2_tiles()
+
+# Return "relevant" dependency versions
+message("Version of package 'sf': ",packageVersion("sf"))
+message("Version of package 's2': ",packageVersion("s2"))
+message("Version of package 'wk': ",packageVersion("wk"))
