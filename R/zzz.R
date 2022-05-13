e1eb0f588a556dc4c79515d42a473588132acd57
Luigi Ranghetti <ggranga@gmail.com>
2022-05-13 17:32:07 +0200

    Alert about baseline 04.00 issue

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,11 @@
   packageStartupMessage(paste(
     "Welcome to sen2r. To use the package from a GUI, launch",
     " > sen2r()",
-    "Documentation: https://sen2r.ranghetti.info",
+    "Documentation: http://sen2r.ranghetti.info",
+    "",
+    "IMPORTANT NOTE: due to changes occurred in SAFE format from baseline 04.00,",
+    "sen2r outputs computed on images newer than 25th January 2022 are wrong.",
+    "Currently it is not possible to announce when this issue will be managed.",
     # if (all(
     #   Sys.info()["sysname"] %in% c("Windows", "Darwin"),
     #   length(load_binpaths()) == 0
